b01d59b192491df2e5774730b1cb1fe25a299b44
PierMario <piermario.salis@aol.com>
2020-12-14 22:57:35 +0100

    End

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// Istruzioni:
+// Stampare a schermo un messaggio all'interno di un h1, utilizzando i data.
+// Bonus: Aggiungere un'immagine presa anch'essa da un data.
+
+// Avvio di VueJS
+
+let start = new Vue({
+  el: '#root',
+  data: {
+    message:"Welcome to VueJS",
+    photo:"https://i1.wp.com/storage.googleapis.com/blog-images-backup/1*wFL3csJ96lQpY0IVT9SE3w.jpeg?ssl=1"
+  }
+});
